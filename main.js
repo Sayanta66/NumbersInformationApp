6fec63cae7e0c636149168f595afa6476453e319
Sayanta Banerjee <53281158+Sayanta66@users.noreply.github.com>
2021-01-23 20:06:24 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('form');
+const factDiv = document.querySelector('.number-fact');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const loadText ="wait a little bit ?";
+  factDiv.innerHTML = loadText;
+  const baseURL = "https://cors-anywhere.herokuapp.com/http://numbersapi.com/";
+  const number = e.target.querySelector('input[type="number"]').value;
+  fetch(baseURL + number,{
+    headers:{
+      'x-requested-with': 'text/plain'
+    }})
+    .then(response => response.text())
+    .then(text => factDiv.innerHTML = text)
+    .catch(e=>console.log(e));
+}) 
